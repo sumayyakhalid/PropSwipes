918d573d5191db74a0687ef77fc19dc9cf8aff34
sumayyakhalid <sumayyakhalid15.2k@gmail.com>
2025-12-01 11:22:47 +0500

    initial commit

---
+++ src/layouts/auth/classic.js
@@ -0,0 +1,117 @@
+import PropTypes from 'prop-types';
+// @mui
+import { alpha, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+// auth
+import { useAuthContext } from 'src/auth/hooks';
+// routes
+import { paths } from 'src/routes/paths';
+import { RouterLink } from 'src/routes/components';
+// hooks
+import { useResponsive } from 'src/hooks/use-responsive';
+// theme
+import { bgGradient } from 'src/theme/css';
+// components
+import Logo from 'src/components/logo';
+
+// ----------------------------------------------------------------------
+
+const METHODS = [
+  {
+    id: 'jwt',
+    label: 'Jwt',
+    path: paths.auth.jwt.login,
+    icon: '/assets/icons/auth/ic_jwt.svg',
+  },
+  {
+    id: 'firebase',
+    label: 'Firebase',
+    path: paths.auth.firebase.login,
+    icon: '/assets/icons/auth/ic_firebase.svg',
+  },
+  {
+    id: 'amplify',
+    label: 'Amplify',
+    path: paths.auth.amplify.login,
+    icon: '/assets/icons/auth/ic_amplify.svg',
+  },
+  {
+    id: 'auth0',
+    label: 'Auth0',
+    path: paths.auth.auth0.login,
+    icon: '/assets/icons/auth/ic_auth0.svg',
+  },
+];
+
+export default function AuthClassicLayout({ children, image, title }) {
+  const { method } = useAuthContext();
+
+  const theme = useTheme();
+
+  const upMd = useResponsive('up', 'md');
+
+  const renderLogo = (
+    <Logo
+      sx={{
+        zIndex: 9,
+        position: 'absolute',
+        m: { xs: 2, md: 5 },
+      }}
+    />
+  );
+
+  const renderContent = (
+    <Stack
+      sx={{
+        width: 1,
+        mx: 'auto',
+        backgroundImage: `url('/assets/illustrations/illustration_dashboard.jpg')`,
+        maxWidth: 480,
+        px: { xs: 2, md: 8 },
+        py: { xs: 15, md: 30 },
+      }}
+    >
+      {children}
+    </Stack>
+  );
+
+  const renderSection = (
+    <Stack
+      flexGrow={1}
+      alignItems="center"
+      justifyContent="center"
+      spacing={10}
+      sx={{
+        borderRadius: '10px',
+        margin: '2rem',
+        backgroundColor: '#016BFF',
+      }}
+    >
+      <Box
+        component="img"
+        alt="auth"
+        src={image || '/assets/illustrations/propState_logo.png'}
+        sx={{ maxWidth: 180 }}
+      />
+    </Stack>
+  );
+
+  return (
+    <Stack component="main" direction="row">
+      {/* {renderLogo} */}
+
+      {renderContent}
+      {upMd && renderSection}
+    </Stack>
+  );
+}
+
+AuthClassicLayout.propTypes = {
+  children: PropTypes.node,
+  image: PropTypes.string,
+  title: PropTypes.string,
+};
---
+++ src/sections/auth/jwt/jwt-login-view.js
@@ -0,0 +1,142 @@
+import * as Yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+// @mui
+import LoadingButton from '@mui/lab/LoadingButton';
+import Link from '@mui/material/Link';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputAdornment from '@mui/material/InputAdornment';
+// routes
+import { Box, Checkbox } from '@mui/material';
+import { paths } from 'src/routes/paths';
+import { RouterLink } from 'src/routes/components';
+import { useSearchParams, useRouter } from 'src/routes/hooks';
+// config
+import { PATH_AFTER_LOGIN } from 'src/config-global';
+// hooks
+import { useBoolean } from 'src/hooks/use-boolean';
+// auth
+import { useAuthContext } from 'src/auth/hooks';
+// components
+import Iconify from 'src/components/iconify';
+import FormProvider, { RHFCheckbox, RHFTextField } from 'src/components/hook-form';
+
+// ----------------------------------------------------------------------
+
+export default function JwtLoginView() {
+  const { login } = useAuthContext();
+
+  const router = useRouter();
+
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const searchParams = useSearchParams();
+
+  const returnTo = searchParams.get('returnTo');
+
+  const password = useBoolean();
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string().required('Email is required').email('Email must be a valid email address'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const defaultValues = {
+    email: 'demo@minimals.cc',
+    password: 'demo1234',
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await login?.(data.email, data.password);
+
+      router.push(returnTo || PATH_AFTER_LOGIN);
+    } catch (error) {
+      console.error(error);
+      reset();
+      setErrorMsg(typeof error === 'string' ? error : error.message);
+    }
+  });
+
+  const renderHead = (
+    <Stack spacing={2} sx={{ mb: 5 }}>
+      <Typography variant="h3">Welcome Back</Typography>
+      <Typography variant="body2">Welcome back! Please enter your detail</Typography>
+    </Stack>
+  );
+
+  const renderForm = (
+    <Stack spacing={2.5}>
+      {!!errorMsg && <Alert severity="error">{errorMsg}</Alert>}
+
+      <RHFTextField name="email" label="Email address" />
+
+      <RHFTextField
+        name="password"
+        label="Password"
+        type={password.value ? 'text' : 'password'}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={password.onToggle} edge="end">
+                <Iconify icon={password.value ? 'solar:eye-bold' : 'solar:eye-closed-bold'} />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <RHFCheckbox name="remember" label="Remember me" />
+
+        <Link
+          variant="body2"
+          color="inherit"
+          underline="always"
+          sx={{ alignSelf: 'flex-end' }}
+          href={paths.authDemo.classic.forgotPassword}
+          component={RouterLink}
+        >
+          Forgot password?
+        </Link>
+      </Box>
+
+      <LoadingButton
+        fullWidth
+        size="large"
+        type="submit"
+        variant="contained"
+        loading={isSubmitting}
+        sx={{
+          backgroundColor: '#016BFF',
+          '&:hover': {
+            backgroundColor: '#0152CC',
+          },
+        }}
+      >
+        Login
+      </LoadingButton>
+    </Stack>
+  );
+
+  return (
+    <FormProvider methods={methods} onSubmit={onSubmit} sx={{ backgroundColor: 'blue' }}>
+      {renderHead}
+      {renderForm}
+    </FormProvider>
+  );
+}
